a64fb451e0709d60fd536e9a578dbf379aa3ee21
eesean <tan.eesean@gmail.com>
2023-05-22 18:23:11 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 import connectDB from './database.js'
+import userRoutes from './userRoute.js'
+import express from 'express'
+import dotenv  from 'dotenv'
 
 connectDB()
-const app = express();
-connectDB();
+
+dotenv.config()
+
+const app = express()
+
+//Create API for user
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+
+//Express js listen method to run project on http://localhost:5000
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
