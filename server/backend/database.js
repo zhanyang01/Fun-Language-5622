ccd8af123fcda11fa21c4617dd3c374ee679eea6
eesean <tan.eesean@gmail.com>
2023-05-22 14:16:08 +0800

    commit

---
+++ server/backend/database.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        //database Name
+        const databaseName='demomern';
+        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+        console.log(`Database connected : ${con.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
