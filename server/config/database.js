1a190087f509629bad755ff6c1afa3b725c6a9e3
eesean <tan.eesean@gmail.com>
2023-05-22 16:33:18 +0800

    commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
+import User from '../userModel.js'
 
 const connectDB = async () => {
     try {
-        const databaseName='FunLanguage';
-        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, { 
+        const databaseName = 'FunLanguage';
+        const con = await mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     });
-        console.log(`Database connected : ${con.connection.host}`)
+        console.log(`Connection successful : ${con.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-import express from "express";
-import connectDB from './server/backend/database.js';
+import connectDB from '../database.js'
 
-const app = express();
-connectDB();
+connectDB()
