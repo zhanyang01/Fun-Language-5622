fdb2c4b0293b5a928e22130baf80b5c7555b40ab
zhanyang01 <ngzhanyang01@gmail.com>
2023-05-23 15:15:22 +0800

    vv

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -2,9 +2,9 @@ import React, {Fragment} from 'react';
 
 const Homepage = () => {
     return (
-        <>
+        <Fragment>
             <h1>Welcome to Homepage which is only visible when you are logged in </h1>
-        </>
+        </Fragment>
     )
 }
 export default Homepage
