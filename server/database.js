49e6489060c4ee64c2747d9eea52d23ffdef37db
eesean <tan.eesean@gmail.com>
2023-05-23 10:15:26 +0800

    commit

--- server/database.js
+++ server/database.js
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose'
-import User from './userModel.js'
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
         const databaseName = 'FunLanguage';
         const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
-        console.log(`Connection successful : ${con.connection.host}`)
+        console.log(`Connection successful : ${con.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`)
-        process.exit(1)
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ import userRoutes from './userRoute.js'
 import express from 'express'
 import dotenv  from 'dotenv'
 
-connectDB()
+connectDB();
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 //Create API for user
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000
 
 //Express js listen method to run project on http://localhost:5000
-app.listen(PORT, console.log("Server started"))
+app.listen(PORT, console.log("Server started"));
