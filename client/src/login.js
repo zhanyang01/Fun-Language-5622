6042b63847d51292fb68cbac42f63eaa92392da3
zhanyang01 <ngzhanyang01@gmail.com>
2023-05-23 15:11:32 +0800

    zzz

--- client/src/login.js
+++ client/src/login.js
@@ -4,7 +4,7 @@ import {useHistory} from "react-router-dom"
 const Login = ({setLoginUser}) => {
 const history = useHistory()
     const [user,setUser] = useState({
-        name:"",
+        email:"",
         password: ""
     })
     const handleChange = e =>{
--- client/src/register.js
+++ client/src/register.js
@@ -16,7 +16,7 @@ const Register = () => {
     }
 //register function 
    const egister = ()=>{
-   const {name, username,email,password} = user
+   const {name,username,email,password} = user
    if (name && username && email && password){
     axios.post("http://localhost:6969/Register",user )
     .then(res=>console.log(res))
@@ -40,30 +40,35 @@ const Register = () => {
         <form action="#">
             <div class="flex flex-col mb-2">
                 <div class=" relative ">
-                    <input type="text" id="create-account-pseudo" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="name" value={user.name} onChange={handleChange} placeholder="FullName"/>
+                    <input type="text" id="create-account-name" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="name" value={user.name} onChange={handleChange} placeholder="FullName"/>
                     </div>
                 </div>
-                <div class="flex gap-4 mb-2">
+                <div class="flex flex-col mb-2">
                     <div class=" relative ">
-                        <input type="text" id="create-account-first-name" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="email" value={user.email} onChange={handleChange} placeholder="Email"/>
+                        <input type="text" id="create-account-username" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="username" value={user.username} onChange={handleChange}    placeholder="username"/>
                         </div>
+                    </div>
+                    <div class="flex gap-4 mb-2">
+                        <div class=" relative ">
+                            <input type="text" id="create-account-email" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="email" value={user.email} onChange={handleChange} placeholder="Email"/>
+                            </div>
 
-                        </div>
-                        <div class="flex flex-col mb-2">
-                            <div class=" relative ">
-                                <input type="password" id="create-account-email" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}    placeholder="password"/>
+                            </div>
+                            <div class="flex flex-col mb-2">
+                                <div class=" relative ">
+                                    <input type="password" id="create-account-password" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}    placeholder="password"/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex w-full my-4">
-                                <button type="submit" class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " onClick={egister} >
-                                    Register
-                                </button>
-                            </div>
-                        </form>
+                                <div class="flex w-full my-4">
+                                    <button type="submit" class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " onClick={egister} >
+                                        Register
+                                    </button>
+                                </div>
+                            </form>
 
 
+                                                            </div>
                                                         </div>
-                                                    </div>
 
         </>
     )
