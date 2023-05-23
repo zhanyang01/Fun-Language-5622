2a7f30f8ac3b0b951cbbc8232e287e17e40a897a
eesean <tan.eesean@gmail.com>
2023-05-23 14:52:51 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
-import connectDB from './database.js'
-import userRoutes from './userRoute.js'
-import express from 'express'
-import dotenv  from 'dotenv'
+import connectDB from './database.js';
+import userRoutes from './userRoute.js';
+import express from 'express';
+import dotenv  from 'dotenv';
+// import cors from 'cors';
 
 connectDB();
 
 dotenv.config();
 
 const app = express();
+
+/*
+app.use(express.json());;
+
+app.use(express.urlencoded());
+
+app.use(cors());
+*/
 
 //Create API for user
 app.use('/api/users', userRoutes);
@@ -16,3 +25,39 @@ const PORT = process.env.PORT || 5000
 
 //Express js listen method to run project on http://localhost:5000
 app.listen(PORT, console.log("Server started"));
+
+/*
+app.post("/Login", (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email: email}, (err, user) => {
+        if (user) {
+            if (password === user.password) {
+                res.send({message: "login success", user: user});
+            } else {
+            res.send({message: "wrong credentials"});
+        }
+        } else {
+            res.send("not register");
+        }
+    })
+});
+
+app.post("/Register", (req, res) => {
+    console.log(req.body);
+    const {name, userName, email, password} = req.body;
+    User.findOne({email: email}, (err, user) => {
+        if (user) {
+            res.send({message: "user already exists"});
+        } else {
+            const user = new User({name, userName, email, password});
+            user.save(err => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send({message: "successful"});
+                }
+            })
+        }
+    })
+});
+*/
