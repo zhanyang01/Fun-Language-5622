4fff02338585ebe7b3a9d79a5d1af3762a827c3f
eesean <tan.eesean@gmail.com>
2023-05-23 10:11:51 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -15,4 +15,4 @@ app.use('/api/users', userRoutes)
 const PORT = process.env.PORT || 5000
 
 //Express js listen method to run project on http://localhost:5000
-app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log("Server started"))
