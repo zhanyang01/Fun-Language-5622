1ca08639a2c85cd5acf7e23754e67d259a9be964
eesean <tan.eesean@gmail.com>
2023-05-24 23:32:20 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -24,36 +24,28 @@ const PORT = process.env.PORT || 6969
 
 app.post("/Login", (req, res) => {
     const {email, password} = req.body;
-    User.find({email: email}).then((err, user) => {
-        if (user) {
-            if (password === user.password) {
-                res.send({message: "login success", user: user});
-            } else {
-                res.send({message: "wrong credentials"});
-            }
+    const user = User.find({email: email});
+    if (user) {
+        if (password === user.password) {
+            res.send({message: "login success", user: user});
         } else {
-            res.send("not register");
+            res.send({message: "wrong credentials"});
         }
-    })
+    } else {
+        res.send("not registered");
+    }
 });
 
 app.post("/Register", (req, res) => {
     console.log(req.body);
     const {name, userName, email, password} = req.body;
-    User.find({email: email}).then((err, user) => {
-        if (user) {
-            res.send({message: "user already exists"});
-        } else {
-            // const user = new User({name, userName, email, password});
-            const newUser = User.insertMany([{name, userName, email, password}]).then(err => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.send({message: "successful"})
-                }
-            });
-        }
-    })
+    const user = User.find({email: email});
+    if (user) {
+        res.send({message: "user already exists"});
+    } else {
+        User.insertMany([{name, userName, email, password}]);
+        res.send({message: "successful"})
+    }
 });
 
 // Express js listen method to run project
