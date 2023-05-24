72b7e9890667299a0e40f978e45355fb215dca06
zhanyang01 <ngzhanyang01@gmail.com>
2023-05-24 20:17:12 +0800

    ...

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -59,9 +59,11 @@ const history = useNavigate()
                         </div>
                     </div>
                     <div className="flex w-full">
-                        <button type="submit" className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={login}>
-                            Login
-                        </button>
+                        <Link to="/homepage">
+                            <button type="submit" className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={login}>
+                                Login
+                            </button>
+                        </Link>
                     </div>
                 </form>
             </div>
