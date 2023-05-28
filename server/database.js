cca7d8072261e16e4e71c7aae18ef0f562c44350
zhanyang01 <ngzhanyang01@gmail.com>
2023-05-28 15:42:14 +0800

    .

--- server/database.js
+++ server/database.js
@@ -1,14 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const databaseName = 'FunLanguage';
-        const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
-        console.log(`Connection successful : ${con.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-}
+  try {
+    const databaseName = "FunLanguage";
+    // const con = await mongoose.connect(process.env.MONGODB_URL);
+    const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
+    console.log(`Connection successful : ${con.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import connectDB from './database.js';
-import userRoutes from './userRoute.js';
-import express from 'express';
-import User from './userModel.js';
-import dotenv from 'dotenv';
+import connectDB from "./database.js";
+import userRoutes from "./userRoute.js";
+import express from "express";
+import User from "./userModel.js";
+import dotenv from "dotenv";
 import cors from "cors";
 // import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt";
@@ -20,18 +20,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Create API for user
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 6969
+const PORT = process.env.PORT || 6969;
 
-app.post("/Login", async(req, res) => {
-    const user = User.findOne({email: req.body.email});
-    try {
-        if (user) {
-            const isCorrect = await bcrypt.compare(req.body.password, user.password);
-            if (isCorrect) {
-                res.send({message: "login success"});
-                /*
+app.post("/Login", async (req, res) => {
+  const user = User.findOne({ email: req.body.email });
+  try {
+    if (user) {
+      const isCorrect = await bcrypt.compare(req.body.password, user.password);
+      if (isCorrect) {
+        res.send({ message: "login success" });
+        /*
                 const payload = {name: user.name, userName: user.userName, email: user.email};
                 const token = jwt.sign(payload, process.env.TOKEN_KEY, {expiresIn: 86400});
                 const valid = jwt.verify(token, process.env.TOKEN_KEY);
@@ -41,37 +41,45 @@ app.post("/Login", async(req, res) => {
                     res.send({message: "invalid login"})
                 }
                 */
-            } else {
-                res.status(404).json({message: "wrong credentials"});
-                // res.status(404);
-                // throw new Error("wrong credentials");
-            }
-        } else {
-            res.status(404).json({message: "not registered"});
-            // res.status(404);
-            // throw new Error("not registered");
-        }
-    } catch(e) {
-        console.log(e);
+      } else {
+        res.status(404).json({ message: "wrong credentials" });
+        // res.status(404);
+        // throw new Error("wrong credentials");
+      }
+    } else {
+      res.status(404).json({ message: "not registered" });
+      // res.status(404);
+      // throw new Error("not registered");
     }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.post("/Register", async(req, res) => {
-    console.log(req.body);
-    const user = User.findOne({email: req.body.email});
-    try {
-        if (user) {
-            res.status(404).json({message: "user already exists"});
-            // res.status(404);
-            // throw new Error("user already exists");
-        } else {
-            user.password = await bcrypt.hash(req.body.password, 10);
-            await User.insertMany([req.body]);
-            res.send({message: "successful"});
-        }
-    } catch(e) {
-        console.log(e);
+app.post("/Register", async (req, res) => {
+  console.log(req.body);
+  const { email, password, name, username } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      console.log("user exists");
+      res.status(404).json({ message: "user already exists" });
+      // res.status(404);
+      // throw new Error("user already exists");
+    } else {
+      console.log("all seems OK");
+      const encryptedPass = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User({ email, password: encryptedPass, name, username });
+      await User.create(newUser).then(() => {
+        res.send({ message: "successful" });
+      });
+      // await User.insertMany([req.body]);
+      // res.send({ message: "successful" });
     }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // Express js listen method to run project
