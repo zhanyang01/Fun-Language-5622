a89ea4d554c565ecdb460e68b7e07317e416c2b3
zhanyang01 <ngzhanyang01@gmail.com>
2023-06-08 16:24:44 +0800

    refactor: commented out confirm password for now

--- client/src/Pages/Authentication/register.jsx
+++ client/src/Pages/Authentication/register.jsx
@@ -70,11 +70,13 @@ const Register = () => {
                                     <input type="password" id="create-account-password" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}    placeholder="Password"/>
                                     </div>
                                 </div>
+                                {/*
                                 <div class="flex flex-col mb-2">
                                     <div class=" relative ">
                                         <input type="password" id="create-account-password" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="confirm_password" value={user.confirmpassword} onChange={handleChange}    placeholder="Confirm Password"/>
                                         </div>
                                     </div>
+                                    */}
                                     <div class="flex w-full my-4">
                                         <Link to= "/login">
                                             <button type="submit" className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " onClick={registerAccount} >
--- server/server.js
+++ server/server.js
@@ -25,16 +25,16 @@ app.use("/api/users", userRoutes);
 // const PORT = process.env.PORT || 6969;
 const PORT = 6969;
 
-app.post("/Login", async(req, res) => {
-    try {
-        const user = await User.findOne({email: req.body.email});
-        if (user) {
-            // const isCorrect = await bcrypt.compare(req.body.password, user.password);
-            if (req.body.password === user.password) {
-                res.send({message: "login success"});
-                console.log("login success");
-                // res.status(200).json({message: "login success"});
-                /*
+app.post("/Login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      // const isCorrect = await bcrypt.compare(req.body.password, user.password);
+      if (req.body.password === user.password) {
+        res.send({ message: "login success" });
+        console.log("login success");
+        // res.status(200).json({message: "login success"});
+        /*
                 const payload = {name: user.name, userName: user.userName, email: user.email};
                 const token = jwt.sign(payload, process.env.TOKEN_KEY, {expiresIn: 86400});
                 const valid = jwt.verify(token, process.env.TOKEN_KEY);
@@ -44,47 +44,48 @@ app.post("/Login", async(req, res) => {
                     res.send({message: "invalid login"});
                 }
                 */
-            } else {
-                // res.status(400).json({message: "wrong credentials"})
-                res.send({message: "wrong credentials"});
-                console.log("wrong credentials");
-            }
-        } else {
-            res.send({message: "not registered"});
-            console.log("not registered");
-        }
-    } catch(e) {
-        console.log(e);
+      } else {
+        // res.status(400).json({message: "wrong credentials"})
+        res.send({ message: "wrong credentials" });
+        console.log("wrong credentials");
+      }
+    } else {
+      res.send({ message: "not registered" });
+      console.log("not registered");
     }
-  });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-app.post("/Register", async(req, res) => {
-    try {
-        const { name, username, password, email } = req.body;
-        const user = await User.findOne({email: req.body.email});
-        if (user) {
-            res.send({message: "user already exists"});
-            console.log("user already exists");
-        } else {
-            /*
+app.post("/Register", async (req, res) => {
+  try {
+    const { name, username, password, email } = req.body;
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      res.send({ message: "user already exists" });
+      console.log("user already exists");
+    } else {
+      /*
             let newDocument = {
                 name: req.body.name, 
                 userName: req.body.userName, 
                 email: req.body.email,
                 password: req.body.password
+                confirmpassword = req.body.confirmpassword
             };
             */
-            // const encryptedPass = await bcrypt.hash(password, 10);
-            const newUser = new User({ name, username, password, email});
-            await User.create(newUser).then(() => {
-                res.send({message: "successful"});
-                console.log("registration success");
-            });
-        }
-    } catch(e) {
-        console.log(e);
+      // const encryptedPass = await bcrypt.hash(password, 10);
+      const newUser = new User({ name, username, password, email });
+      await User.create(newUser).then(() => {
+        res.send({ message: "successful" });
+        console.log("registration success");
+      });
     }
-  });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 // Express js listen method to run project
 app.listen(PORT, console.log("Server started"));
