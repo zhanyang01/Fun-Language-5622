c5034b7a6601928552dff4d958cd5ec30501835d
Tan Ee Sean <tan.eesean@gmail.com>
2023-06-09 11:34:00 +0800

    profile debugging

--- client/src/Pages/profile.jsx
+++ client/src/Pages/profile.jsx
@@ -12,11 +12,11 @@ const Profile = () => {
         localStorage.getItem("username")
     }
 
-    const hasName = username.length > 0;
+    // const hasName = username.length > 0;
 
     return (
         <>
-            {hasName && <h1> Hello {username} </h1>}
+            <h1> Hello {username} </h1>
             <div className="flex w-full">
                 <Link to="/homepage">
                     <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={homepage}>
