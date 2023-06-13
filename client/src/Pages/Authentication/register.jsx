8fa164f396a786bb77d0cd615b06c16b591bc476
Tan Ee Sean <tan.eesean@gmail.com>
2023-06-13 21:16:34 +0800

    commit

--- client/src/Pages/Authentication/register.jsx
+++ client/src/Pages/Authentication/register.jsx
@@ -87,7 +87,7 @@ const Register = () => {
                             </div>
                             <div class="flex flex-col mb-2">
                                 <div class=" relative ">
-                                    <input type="password" id="create-account-password" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}    placeholder="Password (>= 8 characters)"/>
+                                    <input type="password" id="create-account-password" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}    placeholder="Password(>= 8 characters)"/>
                                     </div>
                                 </div>
                                     <div class="flex w-full my-4">
