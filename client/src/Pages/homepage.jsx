dff870931a2baf3cba43d5665087afaa78c06b0a
zhanyang01 <ngzhanyang01@gmail.com>
2023-06-17 17:56:45 +0800

    feat: add the choosing language features for users

--- client/src/Pages/homepage.jsx
+++ client/src/Pages/homepage.jsx
@@ -4,8 +4,8 @@ import {useNavigate, Link} from 'react-router-dom';
 const Homepage = () => {
     const navigate = useNavigate();
 
-    const english = () => {
-        navigate('/english');
+    const choosinglanguage = () => {
+        navigate('/choosinglanguage');
     }
 
     const profile = () => {
@@ -21,9 +21,9 @@ const Homepage = () => {
         <>
             <h1>Welcome to Fun Language!!! </h1>
             <div className="flex w-full">
-                <Link to="/english">
-                    <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={english}>
-                        English
+                <Link to="/choosinglanguage">
+                    <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={choosinglanguage}>
+                        Please choose a Language to learn!!
                     </button>
                 </Link>
             </div>
