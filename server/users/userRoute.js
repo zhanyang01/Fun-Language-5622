ee3bc1ba9427f9bfc36a2dedbdc8caf0a8d2b9c6
zhanyang01 <ngzhanyang01@gmail.com>
2023-06-18 17:02:03 +0800

    feat: usercontroller + userroute delete user

--- server/users/userRoute.js
+++ server/users/userRoute.js
@@ -1,4 +1,4 @@
-import { getUsers, getUserById } from "./userController.js";
+import { getUsers, getUserById, deleteUser } from "./userController.js";
 import express from "express";
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.route("/").get(getUsers);
 
 // express router method to create route for getting users by id
 router.route("/:id").get(getUserById);
+
+// express router method to create route for deleting a user
+router.route("/:id").delete(deleteUser);
 
 export default router;
