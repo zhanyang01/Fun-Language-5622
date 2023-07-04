5fdbc7522fb63c2518be7dc0e0ad5966c14ef900
Tan Ee Sean <tan.eesean@gmail.com>
2023-07-04 21:40:07 +0800

    commit

--- client/src/Pages/Users/profile.jsx
+++ client/src/Pages/Users/profile.jsx
@@ -317,13 +317,13 @@ const Profile = () => {
             </div>
         <h1> {usern} </h1>
         <h2> Courses completed: </h2>
-        <div className="records">
+        <h4>
         <ol>
                 {records.map(task => (
                     <li key = {task}>{task}</li>
                 ))}
             </ol>
-        </div>
+        </h4>
         <h2> Assessments completed: </h2>
         <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
             <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
