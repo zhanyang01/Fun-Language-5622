ca9cb67918d7053896d630c3fa2f33c671466215
Tan Ee Sean <tan.eesean@gmail.com>
2023-07-08 15:53:44 +0800

    basic course 2

--- client/src/Pages/languages/english/Basic/ebcourse2.jsx
+++ client/src/Pages/languages/english/Basic/ebcourse2.jsx
@@ -1,156 +1,86 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import {useNavigate, Link} from 'react-router-dom';
-import ProgressBar from '../../progressbar';
+// import ProgressBar from '../../progressbar';
+import { QuizStructure } from '../../../../Components/UI/QuizStructure';
+import { allQuestions } from '../../../../data';
+import { useQuiz } from '../../../../Storage/UserStorage';
 
 const EBCourse2 = () => {
+    // to save the quiz attempt
+    const {quiz,saveQuiz} = useQuiz()
     const navigate = useNavigate();
+    const questionLabel = "basicQuestionsPartTwo"
+    const [currentAnswers,setCurrentAnswers] = useState([])
 
-    const basic = () => {
+    const handleAnswerChange = (newAnswer,questionNumber) =>{
+
+        currentAnswers[questionNumber] = newAnswer
+        setCurrentAnswers([...currentAnswers])
+    }
+
+    useEffect(()=>{
+        console.log("ans",currentAnswers)
+    },[currentAnswers])
+
+    useEffect(()=>{
+        console.log("quiz",quiz)
+        const currentAns = []
+        // check if there or no
+        if(quiz.hasOwnProperty(questionLabel)){
+            // have prior attempt
+            console.log("attempt exists =D")
+            for(var i = 0;i < quiz[questionLabel].length; i++){
+                const {questionNo,answerValue} = quiz[questionLabel][i]
+                currentAns.push(answerValue)
+            }
+        }
+        else{
+            // first attempt
+            for(var i = 0;i < allQuestions[questionLabel].length; i++){
+                currentAns.push("-1")
+            }
+        }
+        // quiz[questionLabel]
+        setCurrentAnswers(currentAns)
+    },[])
+
+    /*
+    const saveProgress = () => {
         var email = localStorage.getItem("email");
         const course = email + " English Course";
         const meter = email + " English Meter";
         localStorage.setItem(course, "Basic");
-        console.log("Basic");
-        localStorage.setItem(meter, "25%");
-        console.log("25%");
-        navigate('/englishbasic');
-    }
-
-    var answers = ["B", "C", "A"];
-
-    var submissions = answers.length;
-
-    function getCheckedValue(radioName) {
-        var radios = document.getElementsByName(radioName);
-        for (var x = 0; x < radios.length; x++) {
-            if (radios[x].checked) {
-                return radios[x].value;
-            }
+        //console.log("Basic");
+        localStorage.setItem(meter, "0%");
+        //console.log("0%");
+        // this is where the saveQuiz comes in
+        let quizAnswerArray = []
+        for(var i = 0; i < currentAnswers.length; i ++){
+            //console.log(i,currentAnswers[i])
+            quizAnswerArray.push({
+                questionNo:i,
+                answerValue: currentAnswers[i]
+            })
         }
+        saveQuiz(questionLabel,quizAnswerArray)
+        navigate(`/${backToCourseRoute}`);
     }
-
-    function getScore() {
-        var score  = 0;
-        for (var i = 0; i < submissions; i++) {
-            if (getCheckedValue("question" + i) === answers[i]) {
-                score += 1;
-            }
-        }
-        return score;
-    }
-
-    const nextLevel = () => {
-        var maxScore = "/3 correct"
-        var pass = "Passed! Please proceed to next level";
-        var fail = "Please try again!"
-        var score = getScore();
-        if (score === 3) {
-            alert(pass + "\n" + score + maxScore);
-            navigate('/ebcourse3');
-        } else {
-            alert(fail + "\n" + score + maxScore);
-        }
-    }
-
-    const progress = { bgcolor: "#007FFF", completed: 25 };
-
-    const margin = {
-        margin: 20
-    }
+    */
 
     return (
         <>
-            <h1> Basic Course (Part 2) </h1>
-            <ProgressBar bgcolor = {progress.bgcolor} completed = {progress.completed} />
-            <ul class = "quiz">
-
-                <l1>
-                    <h4>What is a synonym for happy?</h4>
-                    <ul class = "choices">
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question0" value = "A" />
-                                <span> scared </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question0" value = "B" />
-                                <span> cheerful </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question0" value = "C" />
-                                <span> upset </span>
-                            </label>
-                        </li>
-                    </ul>
-                </l1>
-
-                <l1>
-                    <h4>Which word is used to describe movement under water?</h4>
-                    <ul class = "choices">
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question1" value = "A" />
-                                <span> run </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question1" value = "B" />
-                                <span> sleep </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question1" value = "C" />
-                                <span> swim </span>
-                            </label>
-                        </li>
-                    </ul>
-                </l1>
-
-                <l1>
-                    <h4>Which punctuation is used to write a question?</h4>
-                    <ul class = "choices">
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question2" value = "A" />
-                                <span> ? </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question2" value = "B" />
-                                <span> ! </span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type = "radio" name = "question2" value = "C" />
-                                <span> . </span>
-                            </label>
-                        </li>
-                    </ul>
-                </l1>
-
-            </ul>
-            <div className="flex w-full">
-                <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={nextLevel}>
-                    Submit
-                </button>
-            </div>
-            <div className="flex w-full" style = {margin}>
-                <Link to="/englishbasic">
-                    <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={basic}>
-                        Save and exit
-                    </button>
-                </Link>
-            </div>
+           <QuizStructure 
+            quizTitle={"Basic Course (Part 2) "}
+            questionLabel = {questionLabel}
+            questions ={allQuestions[questionLabel]}
+            currentAnswers={currentAnswers}
+            handleAnswerChange ={handleAnswerChange}
+            nextLevelRoute ={"ebcourse3"}
+            previousLevelRoute={"englishbasic"}
+            backToCourseRoute={"englishbasic"}
+            />
         </>
     )
 }
 
-export default EBCourse2
+export default EBCourse1
