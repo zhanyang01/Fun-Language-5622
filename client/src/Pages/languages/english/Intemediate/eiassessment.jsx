fc75e6be99105579fbf9df4c740825ea02e67c26
Tan Ee Sean <tan.eesean@gmail.com>
2023-07-09 22:41:58 +0800

    commit

--- client/src/Pages/languages/english/Intemediate/eiassessment.jsx
+++ client/src/Pages/languages/english/Intemediate/eiassessment.jsx
@@ -11,10 +11,11 @@ const EIAssessment = () => {
     return (
         <>
             <h1> Intermediate Assessment </h1>
+            <h2> Please attempt all questions before submitting! </h2>
             <div className="flex w-full">
                 <Link to="/englishintermediate">
                     <button type="submit" className="py-5 px-5 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={intermediate}>
-                        Save and exit
+                        Submit
                     </button>
                 </Link>
             </div>
