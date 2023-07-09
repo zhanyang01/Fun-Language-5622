1474a187d82bec593675e1a1defbe29baaf596b8
zhanyang01 <ngzhanyang01@gmail.com>
2023-07-09 19:03:14 +0800

    fix: backend of save and exit

--- server/Routes/questionAttemptRoute.js
+++ server/Routes/questionAttemptRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-
+import { getQuestionsById, saveQuizProgress } from "../Controllers/questionAttemptController.js";
 const router = express.Router();
+
+// express router method to create route for getting attempts by id
+router.route("/:id").get(getQuestionsById);
+
+// express router method to create route for saving quiz progress
+router.route("/:questionId").post(saveQuizProgress);
 
 export default router;
