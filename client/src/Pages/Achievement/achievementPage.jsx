788143a30be922717315b391aea46b9623354e18
zhanyang01 <ngzhanyang01@gmail.com>
2023-07-15 17:20:49 +0800

    ..

--- client/src/Pages/Achievement/achievementPage.jsx
+++ client/src/Pages/Achievement/achievementPage.jsx
@@ -1,8 +1,9 @@
-import { Button, Container, Heading} from "@chakra-ui/react"
-import React from "react";
+import { Button, Container, Heading, Text} from "@chakra-ui/react"
+import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AchievementComponentStructure} from "../../Components/UI/achievementComponentStructure"
 import { AchievementList } from "../../Achievements/achievementList";
+import axios from "axios";
 
 export const AchievementPage = () => {
 
@@ -12,11 +13,33 @@ export const AchievementPage = () => {
         navigate('/profile');
     }
 
+    const getAchievements = async() => {
+        const userId = localStorage.getItem('userId');
+        var achievements = [];
+        const loadAchievements = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/api/achievements/${userId}`);
+        console.log(loadAchievements);
+        if (loadAchievements.data.message === "Achievements not found") {
+            console.log("Error loading achievements");
+        // this part is not tested yet
+        } else {
+            console.log("Achievements loaded");
+            achievements = loadAchievements.data.data;
+        }
+        return achievements;
+    }
+
+    useEffect(() => {
+        getAchievements();
+    }, [])
+
     return (
         <>
             <Heading my = {10} fontSize = "3xl" color="teal.500">
                 All Achievements
             </Heading>
+            <Text my={5} fontSize="lg">
+                Total unlocked achievements: {getAchievements.length}/{AchievementList.achievements.length}
+            </Text>
             <Container>
                 {
                     AchievementList.achievements.map((achievement) => {
