0e3ddcda71ebc7534a22a6c171aa8f662d4675e3
Tan Ee Sean <tan.eesean@gmail.com>
2023-07-20 09:26:25 +0800

    test with route and controller

--- server/Controllers/emailController.js
+++ server/Controllers/emailController.js
@@ -1,68 +1,32 @@
-/*
-Blueprint for sending email to user
-*/
+import asyncHandler from "express-async-handler";
+import sendCert from "../HelperFunctions/sendCert.js";
 
-import nodemailer from "nodemailer";
-// import asyncHandler from "express-async-handler";
-
-const sendEmail = async(filename, filepath, email) => {
-  // let testAccount = nodemailer.createTestAccount();
-  let transporter = nodemailer.createTransport({
-    // name: 'gmail',
-    service: 'gmail',
-    /*
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    */
-    auth: {
-      user: process.env.APP_EMAIL,
-      pass: process.env.APP_PASS
-    } /*,
-    tls: {
-      rejectUnauthorized: false
-    }
-    */
-  });
-
-  /*
+export const cert = asyncHandler(async(req, res) => {
   var filename = "";
   var filepath = "";
   const { email, testTitle } = req.body;
+  // console.log(req.body);
 
   // need to account for which assessment to attach the correct pdf
   if (testTitle === "Basic Assessment") {
-    filename = 'English Language Basic Assessment.pdf';
-    filepath = './English Certificates/English Language Basic Assessment.pdf';
+    filename = "English Language Basic Assessment.pdf";
+    filepath = "./English Certificates/English Language Basic Assessment.pdf";
   }
   if (testTitle === "Intermediate Assessment") {
-    filename = 'English Language Intermediate Assessment.pdf';
-    filepath = './English Certificates/English Language Intermediate Assessment.pdf';
+    filename = "English Language Intermediate Assessment.pdf";
+    filepath = "./English Certificates/English Language Intermediate Assessment.pdf";
   }
   if (testTitle === "Advanced Assessment") {
-    filename = 'English Language Advanced Assessment.pdf';
-    filepath = './English Certificates/English Language Advanced Assessment.pdf';
+    filename = "English Language Advanced Assessment.pdf";
+    filepath = "./English Certificates/English Language Advanced Assessment.pdf";
   }
-  */
-  
-  let mail = {
-    from: process.env.APP_EMAIL,
-    to: email,
-    subject: "Certificate of Achievement",
-    text: 'We are pleased to inform that you have received the following certificate for passing your assessment! We look forward to your future accomplishments.',
-    attachments: {
-      filename: filename,
-      path: filepath
-    }
-  }
-    
-  transporter.sendMail(mail, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Email sent" + info.response)
-    }
-  })
-};
 
-export default sendEmail;
+  try {
+    await sendCert(filename, filepath, email).then(() => {
+      res.send({ message: "An email has been sent to you" });
+      console.log("An email has been sent to you");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
--- server/Routes/emailRoute.js
+++ server/Routes/emailRoute.js
@@ -1,9 +1,9 @@
-import { sendEmail } from "../Controllers/emailController.js";
 import express from "express";
+import { cert } from "../Controllers/emailController.js";
 
 const router = express.Router();
 
-// express router method to create route for sending email to user
-router.route("/").post(sendEmail);
+// express router method to create route for sending certificate to user
+router.route("/email").post(cert);
 
 export default router;
