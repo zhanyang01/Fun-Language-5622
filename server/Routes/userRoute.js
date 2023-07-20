f25ee3beb364f83f10fe9005f09783722ca0dda5
Tan Ee Sean <tan.eesean@gmail.com>
2023-07-20 18:32:50 +0800

    edit route

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -19,6 +19,6 @@ router.route("/register").post(register);
 //router.put("/verify/:userId").put(verifyEmail);
 
 // express router method to create route for sending email to user
-router.route("/cert").put(cert);
+router.route("/cert/:userId").put(cert);
 
 export default router;
