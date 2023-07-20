f8c736f5b7cf635cecfbb1dbd2a1d8535ea6a074
zhanyang01 <ngzhanyang01@gmail.com>
2023-07-20 18:11:16 +0800

    feat: a incompleted email verification page

--- client/src/Pages/Authentication/emailVerificationForm.jsx
+++ client/src/Pages/Authentication/emailVerificationForm.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import axios from "axios";
-import { Link} from "react-router-dom";
+import { Link, useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Button, Heading } from "@chakra-ui/react";
 
 const EmailVerificationForm = () => {
-    const {userId } = useParams();
+    const {userId, token } = useParams();
     const [verified, setVerified] = useState(false);
     const [verificationFail, setverificationFail] = useState(false);
 
-    {/*useEffect(() => {
+    {useEffect(() => {
         axios
-            .put(`${process.env.REACT_APP_BACKEND_SERVER}/api/auth/verify/${userId}`)
+            .put(`${process.env.REACT_APP_BACKEND_SERVER}/api/auth/verify/${userId}/${token}`)
             .then((res) => {
-                setVerified(true);
+                if (res.data.success) {
+                    setVerified(true);
+                    setverificationFail(false);
+                } else {
+                    setVerified(false);
+                    setverificationFail(true);
+                }
             })
             .catch((err) => {
+                console.log(err);
+                setVerified(false);
                 setverificationFail(true);
             });
-    */}
+    }, [])};
+    return (
+        <>
+            <Heading>
+                Verify now!
+            </Heading>
+            <Link to="/login">
+                <Button>
+                    Login
+                </Button>
+            </Link>
+        </>
+    )
 }
+
+export default EmailVerificationForm;
