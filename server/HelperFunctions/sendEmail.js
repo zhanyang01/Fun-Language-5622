7385b3bffac7d35e808d82e104861c87bbb6fdd0
zhanyang01 <ngzhanyang01@gmail.com>
2023-07-20 17:45:43 +0800

    jwt token set up(will do the rest tmr)

--- server/HelperFunctions/sendEmail.js
+++ server/HelperFunctions/sendEmail.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (emailType, content) => {
-  const { user, recipient } = content;
+  const { user, token, recipient } = content;
   const { _id, name } = user;
   var emailSubject = "";
   var emailText = "";
@@ -17,7 +17,7 @@ const sendEmail = async (emailType, content) => {
   if (emailType == "verification") {
     emailSubject = "Account Verification";
     console.log(emailSubject);
-    var linkToClick = `${process.env.REACT_APP_FRONTEND_SERVER}/verify/${_id}/`;
+    var linkToClick = `${process.env.REACT_APP_FRONTEND_SERVER}/verify/${_id}/${token}`;
     var emailText = `Hello ${name},\n\nPlease click on the following link to verify your account:\n\n${linkToClick}\n\nIf you did not request this, please ignore this email.\n\nRegards,\n\nThe English Language Team`;
   }
   await transporter.sendMail({
