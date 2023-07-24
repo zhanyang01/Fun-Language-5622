3e52a1744e07912ba23cd7dd5fdee619aeb6454e
zhanyang01 <ngzhanyang01@gmail.com>
2023-07-24 12:29:30 +0800

    feat: landing page testing completed

--- client/cypress/e2e/authentication/01landingpage.cy.js
+++ client/cypress/e2e/authentication/01landingpage.cy.js
@@ -1,6 +1,15 @@
 describe("visitng the landing page", () => {
-  it("passes", () => {
+  it("successfully loads", () => {
     cy.visit("/");
+  });
+  it("allows users to sign in", () => {
+    cy.visit("/");
+    cy.contains("Sign In").click();
+  });
+
+  it("allows users to register", () => {
+    cy.visit("/");
+    cy.contains("Sign Up").click();
   });
 });
 
--- client/cypress/e2e/authentication/02loginpage.cy.js
+++ client/cypress/e2e/authentication/02loginpage.cy.js
@@ -1,5 +1,8 @@
-describe("visitng the login page", () => {
-  it("passes", () => {
+describe("The Login Page", () => {
+  it("sign in valid users", () => {
     cy.visit("/login");
+    cy.get("input[name=email]").type("sample@gmail.com");
+    cy.get("input[name=password]").type("12345678");
+    cy.contains("Login").should("be.enabled").click();
   });
 });
