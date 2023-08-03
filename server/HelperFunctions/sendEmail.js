dd92d2079f9f8680fdaa591e9a3bed32aad2b6d1
zhanyang01 <ngzhanyang01@gmail.com>
2023-08-03 21:42:37 +0800

    fix: remove most console log from backend

--- server/HelperFunctions/sendEmail.js
+++ server/HelperFunctions/sendEmail.js
@@ -15,12 +15,10 @@ const sendEmail = async (emailType, content) => {
   var emailText = "";
   if (emailType == "verification") {
     emailSubject = "Account Verification";
-    console.log(emailSubject);
     var linkToClick = `${process.env.REACT_APP_FRONTEND_SERVER}/verify/${_id}/${token}`;
     var emailText = `Hello ${name},\n\nPlease click on the following link to verify your account:\n\n${linkToClick}\n\nIf you did not request this, please ignore this email.\n\nRegards,\n\nThe Fun Language Team`;
   } else if (emailType == "forgetPassword") {
     emailSubject = "Password Reset";
-    console.log(emailSubject);
     var linkToClick = `${process.env.REACT_APP_FRONTEND_SERVER}/resetpassword/${_id}/${token}`;
     var emailText = `Hello ${name},\n\nPlease click on the following link to reset your password:\n\n${linkToClick}\n\nIf you did not request this, please ignore this email.\n\nRegards,\n\nThe Fun Language Team`;
   }
